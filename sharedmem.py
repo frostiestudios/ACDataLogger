ce53c68e1a27e755c4de78092f447aa35c39b8ee
thememetsar <mattwiner04@gmail.com>
2023-06-07 16:33:01 -0700

    0.0.2

--- sharedmem.py
+++ sharedmem.py
@@ -1,4 +1,5 @@
 from pyaccsharedmemory import accSharedMemory
+from appJar import gui
 
 asm = accSharedMemory()
 sm = asm.read_shared_memory()
@@ -8,6 +9,10 @@ if (sm is not None):
 
     print(f"TRACK:{sm.Static.track}")
     print(sm.Graphics.last_time)
-
+    
+    carmodel = sm.Static.car_model
 asm.close()
+
+app = gui("sharedmemorymanager",useTtk=True)
+app.addLabel("CAR MODEL",carmodel)
 
